8a9a803cb9cd89414d564ab30e8bf6231622125b
Rodrigo <rodrigo19962010@live.com>
2017-03-27 13:23:53 -0300

    Add more data

---
+++ projectname/django-user/bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/aluno/Python2017/projectname/django-user/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.11rc1','django-admin.py'
+__requires__ = 'Django==1.11rc1'
+__import__('pkg_resources').run_script('Django==1.11rc1', 'django-admin.py')
