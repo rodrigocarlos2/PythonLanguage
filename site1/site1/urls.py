c31d98d33dd3ed2522e1c9580d3de603e3d64b49
Rodrigo <rodrigo19962010@live.com>
2017-03-27 15:11:42 -0300

    Add Hello World Project with Python and Django

--- site1/site1/urls.py
+++ site1/site1/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+
+from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home)
 ]
